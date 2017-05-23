9f515eea5aead5f9f7a0277a6074244c387fc46c
pengxuelian001 <15112662433@163.com>
2017-05-23 18:28:55 +0800

    文件

--- application/home/controller/User.php
+++ application/home/controller/User.php
@@ -5,20 +5,61 @@ use think\Db;
 
 class User extends Controller
 {
+
     public function add_Users()
     {
-       // echo 11111;
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods:post');
-        $data['openid']='1212';
-        $data['nickname']='拉多布拉';
-        $data['avatarUrl']='1111';
-        $data['country']='CN';
-        $data['city']='Nanchang';
-        $data['language']="zh_CN";
-        $result=Db::name('user')->insertGetId($data);
-       // echo '<pre>';
-          return json ($result);
+        $json = file_get_contents("php://input");
+        file_put_contents("test.txt", $json);
+        if (empty($json)) {
+            $res['success'] = false;
+            $res['message'] = 'Empty RequestData';
+            return json ($res);
+        }
 
+        $read = json_decode($json,true);
+        if (is_null($read)) {
+            $res['success'] = false;
+            $res['message'] = "json_decode_error";
+            return json ($res);
+           // return null;
+        }
+
+        foreach($read as $userInfo => $oneObj) {
+            foreach ($oneObj as $key => $value) {
+                $data[$key] = $value;
+            }
+           $result=Db::name('user')->insert($data);
+        }
+        return json ($result);
+
+    }
+
+    private function checkRequestData()
+    {
+        $json = file_get_contents("php://input");
+
+        file_put_contents("test.txt", $json);
+
+        if (empty($json)) {
+            $res['success'] = false;
+            $res['message'] = 'Empty RequestData';
+            return json ($res);
+            return null;
+        }
+
+        $read = json_decode($json,true);
+        if (is_null($read)) {
+            $res['success'] = false;
+            $res['message'] = "json_decode_error";
+            return json ($res);
+            return null;
+        }
+
+
+
+
+        return $read;
     }
 }
