35a80c08298c4cc05cb68cfada54d8f49cf08b76
pengxuelian001 <15112662433@163.com>
2017-07-04 10:18:49 +0800

    微信接口

--- application/home/controller/Reports.php
+++ application/home/controller/Reports.php
@@ -6,22 +6,39 @@ use think\Db;
 use think\Cache;
 class Reports extends Controller
 {
-    public function upload(){
-        $arr= $this->request->param();
-        $data['openid']=$arr['openid'];
-        $data['title']=$arr['title'];
-        $data['copy']=$arr['copy'];
-        $data['recipients']=$arr['recipients'];
-        $data['position']=$arr['position'];
-        $id=Db::name('reports')->insertGetId($data);
-        $file = request()->file('file');
-        $info = $file->rule('uniqid')->move(APP_PATH . 'public' . DS . 'uploads'. DS .$id);
-            if($info){
-               // $exclePath = $info->getSaveName();  //获取文件名
-              //  $filename = ROOT_PATH . 'public' . DS . 'uploads' . DS . $exclePath;
+    public function add_Reports(){
+        $read = $this->checkRequestData();
+        $img=$read['imgList'];
+        foreach($img as $v){
+           $dat[]=$v;
+
+        }
+         $arr=implode(',',$dat);
+            $data['openid']=$read['openid'];
+            $data['title']=$read['title'];
+            $data['copy']=$read['copy'];
+            $data['recipients']=$read['recipients'];
+            $data['imgList']=$arr;
+            $data['position']=$read['position'];
+            $id=Db::name('reports')->insertGetId($data);
+            if($id){
                 $res['success'] = true;
                 $res['message'] = "success";
+                return json ($res);
             }
+    }
+    public  function upload(){
+        $arr= $this->request->param();
+        $openid=$arr['openid'];
+        $file = request()->file('file');
+        $info = $file->move(APP_PATH . 'public' . DS . 'uploads'. DS ."$openid",'');
+        if($info){
+             $exclePath = $info->getSaveName();  //获取文件名
+             $filename = ROOT_PATH . 'public' . DS . 'uploads' . DS . $exclePath;
+            print_R($filename);die();
+           // return json ($filename);
+          //  echo $info->getExtension();
+        }
     }
     public function age(){
         $data['username']='pp';
@@ -66,8 +83,8 @@ class Reports extends Controller
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods:post');
         $json = file_get_contents("php://input");
-        file_put_contents("test.txt", $json);
-   // print_r($json);die();
+       // file_put_contents("test.txt", $json);
+    //print_r($json);die();
         if (empty($json)) {
             $res['success'] = false;
             $res['message'] = 'Empty RequestData';
